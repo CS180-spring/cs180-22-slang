73fa5ad39b4d2d432fe2e20a20e84c298e444f2d
lucyann <llucy.ann514@gmail.com>
2023-05-11 13:19:18 -0700

    added generalized playlist

--- src/recommend.py
+++ src/recommend.py
@@ -2,6 +2,13 @@ import pandas as pd
 
 def generalize(input_df):
     generalized = input_df.copy()
-
+    danceability_avg = input_df["danceability"].mean()
+    energy_avg = input_df["energy"].mean()
+    loudness_avg = input_df["loudness"].mean()
+    speechiness_avg = input_df["speechiness"].mean()
+    instrumentalness_avg = input_df["instrumentalness"].mean()
+    liveness_avg = input_df["liveness"].mean()
+    average = pd.concat([danceability_avg, energy_avg, loudness_avg, speechiness_avg, instrumentalness_avg, liveness_avg], join ='outer', axis = 1)
+    return average
 def recommend(generalized):
     general = generalized.copy()
